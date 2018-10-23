ca3eeff9813d0b12808d55e4117583df117a7b89
keshavpriya <keshavpriyaji@gmail.com>
2018-10-23 17:27:50 +0530

    layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {UserModule} from "./user/user.module";
+import { HeaderComponent } from './layout/header/header.component';
+import { LeftSidebarComponent } from './layout/left-sidebar/left-sidebar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftSidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
