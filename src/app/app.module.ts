7ff028ae5996e9f1e7244bcf2e715dec5ce71bec
keshavpriya <keshavpriyaji@gmail.com>
2018-10-23 13:23:12 +0530

    adduser component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AdduserComponent } from './adduser/adduser.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdduserComponent
   ],
   imports: [
     BrowserModule
